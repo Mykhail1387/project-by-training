6068e8537b33d6686c039f9128510d64afd0959a
Mykhail1387 <melnychukmykhailo87@gmail.com>
2020-03-07 12:34:18 +0200

    Updates

---
+++ precache-manifest.f197359554845856ed825129941f0de5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "204b129c3b2d5c4263befe553ae9da1a",
+    "url": "/project-by-training/index.html"
+  },
+  {
+    "revision": "83134f4e510ad31eaad4",
+    "url": "/project-by-training/static/css/main.b2b50ce2.chunk.css"
+  },
+  {
+    "revision": "f7539dc3a898aefd0f74",
+    "url": "/project-by-training/static/js/2.9b7f6a90.chunk.js"
+  },
+  {
+    "revision": "83134f4e510ad31eaad4",
+    "url": "/project-by-training/static/js/main.09062845.chunk.js"
+  },
+  {
+    "revision": "31a09f7e039e97e3b9a4",
+    "url": "/project-by-training/static/js/runtime-main.573ece19.js"
+  }
+]);
